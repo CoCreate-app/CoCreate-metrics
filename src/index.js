d5548d4e4bd60d49a3b2defba50cfca8dbe6c9f3
frankpagan <frank@cocreate.app>
2023-09-17 10:44:28 -0600

    feat: debounce balanceUpdate and transactions per organization

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ class CoCreateMetrics {
         this.wsManager = crud.wsManager;
         this.crud = crud;
         this.init();
+        this.organizations = new Map()
     }
 
     init() {
@@ -11,122 +12,170 @@ class CoCreateMetrics {
         }
     }
 
+
     async setBandwidth({ type, data, organization_id }) {
         try {
-            let timeStamp = new Date();
-            let dataDirection = type || 'in'
-            let dataTransfered = 0;
+            let dataTransfered = this.getBytes(data)
 
-            if (typeof data === 'number') {
-                dataTransfered = data;
-            } else if (data instanceof Buffer) {
-                dataTransfered = data.byteLength;
-            } else if (data instanceof String || typeof data === 'string') {
-                dataTransfered = Buffer.byteLength(data, 'utf8');
-            } else if (typeof data === 'object') {
-                const jsonString = JSON.stringify(data);
-                dataTransfered = Buffer.byteLength(jsonString, 'utf8');
-            }
             if (!dataTransfered || !organization_id)
                 return
+            // TODO: set duration 
+            let org = this.organizations.get(organization_id)
+            if (!org) {
+                let self = this
 
-            dataTransfered += 1500; // bytes used for handeling transaction and balance. 
-            dataTransfered = dataTransfered / 1073741824;
-            dataTransfered = dataTransfered.toFixed(32);
-            dataTransfered = parseFloat(dataTransfered);
+                org = {}
+                org.debounce = setTimeout(() => {
+                    self.send(org, organization_id); // Call the callback to display the count
+                    self.organizations.delete(organization_id)
+                }, 60000);
 
-            const platformOrganization = this.crud.config.organization_id
-            let organization = await this.crud.send({
-                method: 'read.object',
-                array: 'organizations',
-                object: { _id: organization_id },
-                organization_id: platformOrganization
-            })
+                org.dataTransferedIn = 0;
+                org.dataTransferedInCount = 0;
+                org.dataTransferedOut = 0;
+                org.dataTransferedOutCount = 0;
 
-            if (organization && organization.object && organization.object[0]) {
-                organization = organization.object[0]
+                this.organizations.set(organization_id, org)
             }
 
-            if (organization.balance <= 0) {
-                this.wsManager.organizations.set(organization_id, false)
-            } else
-                this.wsManager.organizations.set(organization_id, true)
-
-            let isExpired = false
-            if (organization.lastDeposit) {
-                let lastDeposit = new Date(organization.lastDeposit)
-                isExpired = lastDeposit <= timeStamp.setFullYear(timeStamp.getFullYear() - 1)
+            if (type === 'in') {
+                org.dataTransferedIn = dataTransfered;
+                org.dataTransferedInCount++
+            } else if (type === 'out') {
+                org.dataTransferedOut = dataTransfered;
+                org.dataTransferedOutCount++
+            } else {
+                console.log('else')
             }
-
-            let isResetDataTransfer = false
-            if (organization.modified.on) {
-                let previousTimeStamp = new Date(organization.modified.on)
-                if (previousTimeStamp.getMonth() !== timeStamp.getMonth()) {
-                    isResetDataTransfer = true
-                    this.crud.send({
-                        method: 'create.object',
-                        array: 'transactions',
-                        object: {
-                            organization_id,
-                            type: "withdrawal", // deposit, credit, withdrawal, debit
-                            dataTransfered: organization.dataTransfered,
-                            previousTimeStamp
-                        },
-                        organization_id: platformOrganization,
-                        timeStamp
-                    });
-                    organization.dataTransfered = 0
-                }
-            }
-
-            let rate = this.getRate(organization.dataTransfered)
-            let amount = dataTransfered * rate
-            amount = -amount
-            amount = amount.toFixed(32)
-            amount = parseFloat(amount)
-
-            let balanceUpdate = {
-                method: 'update.object',
-                array: 'organizations',
-                object: { _id: organization_id },
-                organization_id: platformOrganization,
-                timeStamp
-            }
-
-            if (isExpired)
-                balanceUpdate.object['balance'] = 0
-            else
-                balanceUpdate.object.$inc = { balance: amount }
-
-            if (isResetDataTransfer)
-                balanceUpdate.object['dataTransfered'] = 0
-            else if (!balanceUpdate.object.$inc)
-                balanceUpdate.object.$inc = { dataTransfered }
-            else
-                balanceUpdate.object.$inc.dataTransfered = dataTransfered
-
-            this.crud.send(balanceUpdate)
-
-            let transaction = {
-                method: 'create.object',
-                array: 'transactions',
-                object: {
-                    organization_id,
-                    type: "withdrawal", // deposit, credit, withdrawal, debit
-                    amount,
-                    rate,
-                    dataDirection,
-                    dataTransfered,
-                },
-                organization_id,
-                timeStamp
-            }
-
-            this.crud.send(transaction);
 
         } catch (error) {
             console.log('Metrics error', error)
         }
+    }
+
+
+    async send(org, organization_id) {
+        delete org.debounce
+
+        org.dataTransferedOut += 250
+        org.dataTransferedOutCount++
+
+        const platformOrganization = this.crud.config.organization_id
+        let organization = await this.crud.send({
+            method: 'read.object',
+            array: 'organizations',
+            object: { _id: organization_id },
+            organization_id: platformOrganization
+        })
+
+        org.dataTransferedIn += this.getBytes(platformOrganization)
+        org.dataTransferedInCount++
+
+        if (organization && organization.object && organization.object[0]) {
+            organization = organization.object[0]
+        } else return
+
+        //TODO: if (organization.transactionInterval) // set in global map to use with timeout 1 sec intervals to 3600 sec
+        if (organization.balance <= 0) {
+            this.wsManager.organizations.set(organization_id, false)
+        } else
+            this.wsManager.organizations.set(organization_id, true)
+
+        let timeStamp = new Date();
+        let isExpired = false
+        if (organization.lastDeposit) {
+            let lastDeposit = new Date(organization.lastDeposit)
+            isExpired = lastDeposit <= timeStamp.setFullYear(timeStamp.getFullYear() - 1)
+        }
+
+        let isResetDataTransfer = false
+        if (organization.modified.on) {
+            let previousTimeStamp = new Date(organization.modified.on)
+            if (previousTimeStamp.getMonth() !== timeStamp.getMonth()) {
+                isResetDataTransfer = true
+                this.crud.send({
+                    method: 'create.object',
+                    array: 'transactions',
+                    object: {
+                        organization_id,
+                        type: "withdrawal", // deposit, credit, withdrawal, debit
+                        dataTransfered: organization.dataTransfered,
+                        previousTimeStamp: previousTimeStamp.toISOString()
+                    },
+                    organization_id: platformOrganization,
+                    timeStamp: timeStamp.toISOString()
+                });
+                organization.dataTransfered = 0
+            }
+        }
+
+        let dataTransfered = org.dataTransferedIn + org.dataTransferedOut
+        dataTransfered = dataTransfered / 1073741824;
+        dataTransfered = dataTransfered.toFixed(32);
+        dataTransfered = parseFloat(dataTransfered);
+
+        console.log('sending: ', dataTransfered, org)
+
+        let rate = this.getRate(organization.dataTransfered)
+        let amount = dataTransfered * rate
+        amount = -amount
+        amount = amount.toFixed(32)
+        amount = parseFloat(amount)
+
+        timeStamp = timeStamp.toISOString()
+        let balanceUpdate = {
+            method: 'update.object',
+            array: 'organizations',
+            object: { _id: organization_id },
+            organization_id: platformOrganization,
+            timeStamp
+        }
+
+        if (isExpired)
+            balanceUpdate.object['balance'] = 0
+        else
+            balanceUpdate.object.$inc = { balance: amount }
+
+        if (isResetDataTransfer)
+            balanceUpdate.object['dataTransfered'] = 0
+        else if (!balanceUpdate.object.$inc)
+            balanceUpdate.object.$inc = { dataTransfered }
+        else
+            balanceUpdate.object.$inc.dataTransfered = dataTransfered
+
+        // console.log('balanceUpdate: ', balanceUpdate)
+
+        this.crud.send(balanceUpdate)
+
+        let transaction = {
+            method: 'create.object',
+            array: 'transactions',
+            object: {
+                organization_id,
+                type: "withdrawal", // deposit, credit, withdrawal, debit
+                amount,
+                rate,
+                dataTransfered,
+                ...org
+            },
+            organization_id,
+            timeStamp
+        }
+
+        this.crud.send(transaction);
+
+    }
+
+    getBytes(data) {
+        if (typeof data === 'number')
+            return data;
+        else if (data instanceof Buffer)
+            return data.byteLength || 0;
+        else if (data instanceof String || typeof data === 'string')
+            return Buffer.byteLength(data, 'utf8') || 0;
+        else if (typeof data === 'object')
+            return Buffer.byteLength(JSON.stringify(data), 'utf8') || 0;
+        else return 0
     }
 
     getRate(totalUsage = 0) {
