92f2c8e0025f461ce8419c0c57e7508e52726934
frankpagan <frank@cocreate.app>
2023-11-26 05:15:29 -0600

    feat: setInterval using worker count

--- src/index.js
+++ src/index.js
@@ -9,10 +9,27 @@ class CoCreateMetrics {
 
     init() {
         // Read memory and CPU info
+        const workerId = process.env.WORKER_ID;
+        const baseInterval = 1000; // Base interval, e.g., 1 second
+        const delay = (workerId * baseInterval) + (this.wsManager.cluster.totalWorkers * baseInterval);
         setInterval(() => {
-            this.readMemoryInfo();
-            this.readCpuInfo();
-        }, 1000);
+            this.readMetrics()
+        }, delay);
+    }
+
+    readMetrics() {
+        this.readMemoryInfo();
+        const cpuInfo1 = this.readCpuInfo();
+        setTimeout(() => {
+            const cpuInfo2 = this.readCpuInfo();
+            const cpuUsage = this.calculateCpuUsage(cpuInfo1, cpuInfo2);
+            let cpuTotalUsage = 0
+            cpuUsage.forEach(coreUsage => {
+                cpuTotalUsage += coreUsage.usage
+            });
+            cpuTotalUsage = cpuTotalUsage / cpuUsage.length
+            console.log(`CPU usage: ${cpuTotalUsage.toFixed(2)}%}`);
+        }, 1000); // 1-second interval
     }
 
     // Function to parse and display memory info
@@ -26,20 +43,52 @@ class CoCreateMetrics {
             }
             return info;
         }, {});
-        console.log('Memory Info:', memInfo);
+        const totalMemory = parseInt(memInfo['MemTotal'], 10);
+        const availableMemory = parseInt(memInfo['MemAvailable'] || memInfo['MemFree'], 10); // Some systems use 'MemFree'
+        const usedMemory = totalMemory - availableMemory;
+        const usedMemoryPercentage = (usedMemory / totalMemory) * 100;
+
+        // console.log(`Total Memory: ${totalMemory} kB`);
+        // console.log(`Used Memory: ${usedMemory} kB`);
+        console.log(`Memory Usage: ${usedMemoryPercentage.toFixed(2)}%`);
     };
 
     // Function to read CPU info (simplified)
     readCpuInfo() {
         const cpuInfoContent = fs.readFileSync('/proc/stat', 'utf8');
         const cpuLines = cpuInfoContent.split('\n');
-        const cpuLine = cpuLines.find(line => line.startsWith('cpu '));
-        if (cpuLine) {
-            // Example processing; more needed for actual CPU usage calculation
-            const cpuTimes = cpuLine.split(' ').slice(1).map(Number);
-            console.log('CPU Times:', cpuTimes);
-        }
+        const cpuInfo = cpuLines
+            .filter(line => line.startsWith('cpu'))
+            .map(line => {
+                const parts = line.split(' ').filter(p => p.trim() !== '');
+                return {
+                    core: parts[0],
+                    times: parts.slice(1).map(Number),
+                };
+            });
+        return cpuInfo;
     };
+
+    calculateCpuUsage(cpuInfo1, cpuInfo2) {
+        const cpuUsage = cpuInfo1.map((coreInfo1, index) => {
+            const coreInfo2 = cpuInfo2[index];
+            const idle1 = coreInfo1.times[3];
+            const idle2 = coreInfo2.times[3];
+
+            const total1 = coreInfo1.times.reduce((acc, val) => acc + val, 0);
+            const total2 = coreInfo2.times.reduce((acc, val) => acc + val, 0);
+
+            const totalDiff = total2 - total1;
+            const idleDiff = idle2 - idle1;
+
+            return {
+                core: coreInfo1.core,
+                usage: (1 - idleDiff / totalDiff) * 100,
+            };
+        });
+
+        return cpuUsage;
+    }
 
 }
 
